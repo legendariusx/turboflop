e26e876ce3287e04a466825160dd289c832bb495
LegenDarius <darius@ddegel.ch>
2025-05-02 12:48:50 +0200

    implement basic spacetimedb database and set up connection

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,34 +1,66 @@
-use spacetimedb::{ReducerContext, Table};
+use spacetimedb::{table, reducer, Table, ReducerContext, Identity};
 
-#[spacetimedb::table(name = person)]
-pub struct Person {
-    name: String
+#[table(name = user, public)]
+pub struct User {
+    #[primary_key]
+    identity: Identity,
+    name: String,
+    online: bool,
 }
 
 #[spacetimedb::reducer(init)]
 pub fn init(_ctx: &ReducerContext) {
     // Called when the module is initially published
+    log::info!("Module registered")
 }
 
-#[spacetimedb::reducer(client_connected)]
-pub fn identity_connected(_ctx: &ReducerContext) {
-    // Called everytime a new client connects
-}
-
-#[spacetimedb::reducer(client_disconnected)]
-pub fn identity_disconnected(_ctx: &ReducerContext) {
-    // Called everytime a client disconnects
-}
-
-#[spacetimedb::reducer]
-pub fn add(ctx: &ReducerContext, name: String) {
-    ctx.db.person().insert(Person { name });
-}
-
-#[spacetimedb::reducer]
-pub fn say_hello(ctx: &ReducerContext) {
-    for person in ctx.db.person().iter() {
-        log::info!("Hello, {}!", person.name);
+#[reducer(client_connected)]
+// Called when a client connects to a SpacetimeDB database server
+pub fn client_connected(ctx: &ReducerContext) {
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        // If this is a returning user, i.e. we already have a `User` with this `Identity`,
+        // set `online: true`, but leave `name` and `identity` unchanged.
+        ctx.db.user().identity().update(User { online: true, ..user });
+    } else {
+        // If this is a new user, create a `User` row for the `Identity`,
+        // which is online, but hasn't set a name.
+        ctx.db.user().insert(User {
+            identity: ctx.sender,
+            name: String::from(""),
+            online: true,
+        });
     }
-    log::info!("Hello, World!");
+}
+
+#[reducer(client_disconnected)]
+// Called when a client disconnects from SpacetimeDB database server
+pub fn identity_disconnected(ctx: &ReducerContext) {
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User { online: false, ..user });
+    } else {
+        // This branch should be unreachable,
+        // as it doesn't make sense for a client to disconnect without connecting first.
+        log::warn!("Disconnect event for unknown user with identity {:?}", ctx.sender);
+    }
+}
+
+#[reducer]
+/// Clients invoke this reducer to set their user names.
+pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
+    let name = validate_name(name)?;
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User { name: name, ..user });
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
+}
+
+/// Takes a name and checks if it's acceptable as a user's name.
+fn validate_name(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("Names must not be empty".to_string())
+    } else {
+        Ok(name)
+    }
 }
